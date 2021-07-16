129491bc9362a7bd333835f69fb208c81291c4e7
Matheus Mendes <matheushenriquesad@gmail.com>
2021-07-16 20:47:45 -0300

    Class 01 - Firsts steps

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,3 +1,12 @@
 fun main(){
-    print("Hello, world!");
+    println("Welcome to Bytebank");
+
+    val user: String = "Matheus";
+    val accountNumber: Int= 375069;
+    var accountBalance: Double = 0.0;
+    accountBalance = 100 + 2.0;
+    accountBalance+=150;
+    println("Welcome ${user}!");
+    println("Account: $accountNumber");
+    println("Account balance: $ $accountBalance");
 }
